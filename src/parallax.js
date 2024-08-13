b65954a0aae9c11454deeac53612a4ef14254028
teentitansccg <teentitansccg@gmail.com>
2024-08-13 08:33:11 -0400

    implementing parallax

---
+++ src/parallax.js
@@ -0,0 +1,19 @@
+// Parallax
+// http://www.jqueryscript.net/animation/Mouse-aware-Parallax-Effect-with-jQuery-and-TweenMax-Mouse-Parallax.html
+$.fn.parallax = function (resistance, mouse) {
+    $el = $(this);
+    TweenLite.to($el, 0.2,
+        {
+            x: -((mouse.clientX - (window.innerWidth / 2)) / resistance),
+            y: -((mouse.clientY - (window.innerHeight / 2)) / resistance)
+        });
+
+};
+
+$(document).mousemove(function (c) {
+    /*$( '#start' ).parallax( 0, c );*/
+    $('#ravenPurple').parallax(-20, c);
+    $('#start-amber').parallax(-30, c);
+    $('#start-stella').parallax(-50, c);
+    $('#start-kaeya').parallax(-60, c);
+});
